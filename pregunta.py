06d9254eaa5e1f8f598ea354b25069216308d787
Andrés Mauricio Zapata Rincón <amzapatari@unal.edu.co>
2023-10-07 09:47:48 -0500

    Lab 5

--- pregunta.py
+++ pregunta.py
@@ -10,15 +10,8 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
-
-    df.dropna(inplace=True)
-    df = df.apply(lambda x: x.astype(str).str.replace("_","-").str.replace("-"," ").str.replace("$","").str.replace(",",""))
-    df = df.apply(lambda x: x.astype(str).str.lower().str.strip(" "))
-    df.fecha_de_beneficio = pd.to_datetime(df["fecha_de_beneficio"],format='mixed', dayfirst = True)
-    df.monto_del_credito = df.monto_del_credito.astype(float)
-    df.drop_duplicates(inplace=True)
+   
 
     return df
 
-#print(df.barrio.value_counts().to_list())
+
