118a5e9ddb867cb01e53f0cbb67bc70e695b8860
Andrés Mauricio Zapata Rincón <amzapatari@unal.edu.co>
2023-10-13 20:56:41 -0500

    Taller data cleaning

--- pregunta.py
+++ pregunta.py
@@ -9,9 +9,31 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 import pandas as pd
 
 def clean_data():
+    
 
-   
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
+
+
+    #borrar todas las filas con espacios vacios
+    df.dropna(inplace=True)
+
+    #convertir todo a minisculas
+    df.loc[:, df.dtypes=='object']=df.loc[:, df.dtypes=='object'].apply(lambda row: row.str.lower())
+
+    #reemplazar
+    df=df.apply(lambda x: x.astype(str).str.replace("-"," ").str.replace("_"," ").str.replace("$","").str.replace(",",""))
+
+    #covertir a fecha
+    df.fecha_de_beneficio = pd.to_datetime(df['fecha_de_beneficio'],format="mixed", dayfirst=True)
+
+    #convertir float
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+
+    #borrar los duplicados
+    df.drop_duplicates(inplace=True) 
 
     return df
+
+
 
 
